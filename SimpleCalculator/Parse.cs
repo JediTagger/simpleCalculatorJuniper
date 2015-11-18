ad27fd6d0b0f867dbd7d157999cb7852997ee1b5
Jesse Turner <jesseturner@comcast.net>
2015-11-18 12:47:02 -0800

    initial commit. Set up Parse class.  Started extracting parts of expression

---
+++ SimpleCalculator/Parse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Parse
+    {
+        public string ExtractOperator(string input)
+        {
+            char[] delimiterChar = { ' ' };
+            string[] expressionElements = input.Split(delimiterChar);
+            string returnOperator = expressionElements[1];
+            return returnOperator;
+        }
+
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using SimpleCalculator;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleCalculator
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+{ 
+     class Program
+     { 
+         static void Main(string[] args)
+         {
+         } 
+     }
+} 
+
